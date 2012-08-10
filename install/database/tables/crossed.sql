47787a0fbc7efd8b9c94a4942e5f3c085357ba3f
Vasily <vbezruchkin@intelliants.com>
2012-08-10 17:28:40 +0600

    #19 fixed. MySQL database structure + data updated.

--- install/database/tables/crossed.sql
+++
@@ -1,9 +0,0 @@
-{install:drop_tables}DROP TABLE IF EXISTS `{install:prefix}crossed`;
-CREATE TABLE `{install:prefix}crossed` (
-  `id` int(8) unsigned NOT NULL auto_increment,
-  `category_id` int(8) unsigned NOT NULL default '0',
-  `category_title` VARCHAR( 255 ) NOT NULL,
-  `crossed_id` int(8) unsigned NOT NULL default '0',
-  PRIMARY KEY  (`id`),
-  KEY `category_id` (`category_id`,`crossed_id`)
-) {install:mysql_version};
--- install/database/tables/votes.sql
+++
@@ -1,9 +0,0 @@
-{install:drop_tables}DROP TABLE IF EXISTS `{install:prefix}votes`;
-CREATE TABLE `{install:prefix}votes` (
-	`listing_id` int(8) NOT NULL default '0',
-	`vote_value` tinyint(4) NOT NULL default '0',
-	`ip_address` varchar(15) NOT NULL,
-	`date` datetime NOT NULL default '0000-00-00 00:00:00',
-	KEY `ip_address` (`ip_address`),
-	KEY `listing_id` (`listing_id`)
-) {install:mysql_version};

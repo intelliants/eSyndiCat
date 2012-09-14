7deb21642746974ac507499a9606fb16edacca4c
Vasily Bezruchkin <vbezruchkin@intelliants.com>
2012-09-14 11:42:35 +0600

    Copyrights changed. #21. Ignores added.

--- includes/util.php
+++ includes/util.php
@@ -1,5 +1,34 @@
 <?php
-//##copyright##
+/******************************************************************************
+ *
+ *	 COMPANY: Intelliants LLC
+ *	 PROJECT: eSyndiCat Directory Software
+ *	 VERSION: 1.7 [Cushy]
+ *	 LISENSE: http://www.esyndicat.com/license.html
+ *	 http://www.esyndicat.com/
+ *
+ *	 This program is a limited version. It does not include the major part of 
+ *	 the functionality that comes with the paid version. You can purchase the
+ *	 full version here: http://www.esyndicat.com/order.html
+ *
+ *	 Any kind of using this software must agree to the eSyndiCat license.
+ *
+ *	 Link to eSyndiCat.com may not be removed from the software pages without
+ *	 permission of the eSyndiCat respective owners.
+ *
+ *	 This copyright notice may not be removed from source code in any case.
+ *
+ *	 Useful links:
+ *	 Installation Manual:	http://www.esyndicat.com/docs/install.html
+ *	 eSyndiCat User Forums: http://www.esyndicat.com/forum/
+ *	 eSyndiCat Helpdesk:	http://www.esyndicat.com/desk/
+ *
+ *	 Intelliants LLC
+ *	 http://www.esyndicat.com
+ *	 http://www.intelliants.com
+ *
+ ******************************************************************************/
+
 
 /*
 	These classes are defined mostly as namespace seperators
